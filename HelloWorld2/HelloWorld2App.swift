d89cbb9427939bf315ce6d442e22f0e9339344c2
PhilippeMeleard <pmeleard1001@gmail.com>
2023-11-27 15:30:29 +0100

    Initial Commit

---
+++ HelloWorld2/HelloWorld2App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld2App.swift
+//  HelloWorld2
+//
+//  Created by Philippe Méléard on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
